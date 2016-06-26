d1d52722bebd96ff55b4ee21e28c41e61a443e92
Ryan Chang <ry4nch4ng@gmail.com>
2016-06-25 20:31:59 -0700

    playlist(limit: 10)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,26 @@ class UsersController < ApplicationController
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     # Now you can access user's private data, create playlists and much more
 
-    playlists = spotify_user.playlists(limit: 20)
+    name = spotify_user.email
+    playlists = spotify_user.playlists(limit: 10)
+    artists = []
+    genres = []
+
+    playlists.each do |pl|
+      pl.tracks.each do |track|
+        unless artists.include?(track.artists.first.name)
+          artists << track.artists.first.name
+          genres << track.artists.first.genres.first if !track.artists.first.genres.empty?
+        end
+      end
+    end
+
+    user = User.new(name: name)
+    user.save
+
+    @name = name
+    @playlists = playlists
+    @artists = artists
+    @genres = genres
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :playlists
   has_many :artists, through: :playlists
+  has_many :genres, through: :artists
+  validates_uniqueness_of :name
 end
